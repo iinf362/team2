652ce3b66fea18b05907a01b614789d44b5429ed
Ethan Yehuda <Ethan3600@gmail.com>
2014-12-16 15:44:28 -0500

    testing javascript

--- js/main.js
+++ js/main.js
@@ -6,3 +6,8 @@ $("button").click(function(){
   $("p").slideToggle();
 });
 */
+$(document).ready(function(){
+	$("#what-is-an-eviction").onclick(function(){
+		$("what-is-an-eviction-content").slideToggle();
+	});
+});
