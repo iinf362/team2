989871f1e516cbe30577e4684237ba04154f294f
Mickeytarian <amikhitarian@albany.edu>
2014-12-16 22:38:24 -0500

    Update main.js
    removed the comment-out

--- js/main.js
+++ js/main.js
@@ -1,11 +1,5 @@
 
-/*
-THIS IS THE GENERAL CODE FOR THE .slideToggle FUNCTION
 
-$("button").click(function(){
-  $("p").slideToggle();
-});
-*/
 $(document).ready(function(){
 	$("button").click(function(){
 		$(".drop-down-content").slideToggle();
