46dc0429ed583a2d5f83d008187479bfe2bae803
Ethan Yehuda <Ethan3600@gmail.com>
2014-12-16 20:55:29 -0500

    dfskjl

--- js/main.js
+++ js/main.js
@@ -6,7 +6,13 @@ $("button").click(function(){
   $("p").slideToggle();
 });
 */
-$(".drop-down-content").hide();
-$("button").click(function(){
-	$(".drop-down-content").show();
+var allDrops = $('.drop-down').hide();
+$('.drop-down-content').click(function() {
+  var wasVisible = $(this).next().is(":visible");
+  if (wasVisible != true){
+    allDrops.slideUp();
+    $(this).next().slideDown();
+  }else {
+    $(this).next().slideUp();
+  }
 });
