e9ae6cd8df8657fff19b2573c9daebe9bff1a7c3
Ethan Yehuda <Ethan3600@gmail.com>
2014-12-16 21:19:32 -0500

    asdf

--- js/main.js
+++ js/main.js
@@ -6,13 +6,6 @@ $("button").click(function(){
   $("p").slideToggle();
 });
 */
-var allDrops = $('.drop-down').hide();
-$('.drop-down-content').click(function() {
-  var wasVisible = $(this).next().is(":visible");
-  if (wasVisible != true){
-    allDrops.slideUp();
-    $(this).next().slideDown();
-  }else {
-    $(this).next().slideUp();
-  }
-});
+$("button").click(function(){
+	$(".drop-down-content").slideToggle();
+	});
